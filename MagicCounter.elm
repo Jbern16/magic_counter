2c14112943670c2517e157b21e63e37093271151
Jbern16 <jbern16@gmail.com>
2016-04-11 20:38:38 -0600

    add list and entry form need to sort out errors

--- MagicCounter.elm
+++ MagicCounter.elm
@@ -6,14 +6,18 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Signal exposing(Address)
 import StartApp.Simple as StartApp
+import String exposing(toInt)
 
 -- MODEL
 
 type alias Model =
   {
     player1 : Int,
-    player2 : Int
-    creatures : List
+    player2 : Int,
+    creatures : List,
+    creatureNameInput : String,
+    healthInput : String,
+    attackInput : String,
     nextID : Int
   }
 
@@ -29,15 +33,18 @@ initialModel =
   {
     player1 =  20,
     player2 =  20,
-    creatures = []
+    creatures = [],
+    creatureNameInput = "",
+    healthInput = "",
+    attackInput = "",
     nextID = 1
   }
 
 newCreature name health attack id =
   {
-    name = name
-    health = health
-    attack = attack
+    name = name,
+    health = health,
+    attack = attack,
     id = id
   }
 
@@ -49,6 +56,9 @@ type Action
   | Increment2
   | Decrement1
   | Decrement2
+  | UpdateCreatureName String
+  | UpdateCreatureHealth String
+  | UpdateCreatureAttack String
   | AddCreature
   | Reset
 
@@ -65,29 +75,83 @@ update action model =
       { model | player1 = model.player1 - 1 }
     Decrement2 ->
       { model | player2 = model.player2 - 1 }
+    UpdateCreatureName contents ->
+      { model | creatureNameInput = contents }
+    UpdateCreatureHealth contents ->
+      { model | healthInput = contents }
+    UpdateCreatureAttack contents ->
+      { model | attackInput = contents }
     AddCreature ->
-
+      let
+        creature = newCreature model.creatureNameInput (parseInt model.healthInput) (parseInt model.attackInput) model.id
+      in
+        { model |
+          creatureNameInput = "",
+          healthInput = "",
+          attackInput = "",
+          creatures = creature :: model.creatures,
+          nextID = model.nextID + 1
+        }
     Reset ->
       { model | player1 = 20, player2 = 20 }
 
 
 -- VIEW
 
+parseInt : String -> Int
+parseInt string =
+  case String.toInt string of
+    Ok value ->
+      value
+    Err error ->
+      0
+
+onInput : Address a -> (String -> a) -> Attribute
+onInput address f =
+  on "input" targetValue (\v -> Signal.message address (f v))
+
+
 creatureList address creature =
-    li [ classList [  ],
-         onClick address (Mark entry.id)
-        ]
-      [ span [ ] [ text creature.name ]
-      [ span [ class "Health" ] [ text creature.health ],
-        span [ class "Attack" ] [ text creature.attack ],
-        button
-          [ class "Delete", onClick address (Delete creature.id) ]
-          [ ]
+      div [] [
+       span [ ] [ text creature.name ],
+       span [ class "Health" ] [ text creature.health ],
+       span [ class "Attack" ] [ text creature.attack ]
       ]
 
-view : Address Action -> Model -> Html
-view address model =
-  div [ class "container" ] [
+newCreatureForm : Address Action -> Model -> Html
+newCreatureForm address model =
+  h4
+   [ ]
+   [ text (model.creatureNameInput) ]
+  div [ ]
+    [ input
+        [ type' "text",
+          value model.creatureNameInput,
+          name "name",
+          onInput address UpdateCreatureName
+          ]
+          [ ],
+      input
+        [ type' "number",
+          value.model.healthInput,
+          name "health",
+          onInput address UpdateCreatureHealth
+          ]
+        [ ],
+      input
+        [ type' "number",
+          value.model.attackInput,
+          name "attack",
+          onInput address UpdateCreatureAttack
+          ]
+        [ ],
+      button
+        [ class "success hollow button",
+          onClick address AddCreature
+        ] [ text "Add Creature" ]
+      ]
+
+player1Health address model =
     div [ class "row" ] [
       div [ class "large-6 columns" ]
       [
@@ -95,28 +159,32 @@ view address model =
         button [ class "success hollow button", onClick address Increment1 ] [ text "+" ],
         h2 [ ] [ text (toString model.player1) ],
         button [ class "alert hollow button", onClick address Decrement1 ] [ text "-" ]
+      ] ]
 
-      ],
-      div [ class "large-6 columns" ]
-      [
-      h3 [ class "subheader" ] [ text "Player 2 Health" ],
-        button [ class "success hollow button", onClick address Increment2 ] [ text "+" ],
-        h2 [  ] [ text (toString model.player2) ],
-        button [ class "alert hollow button", onClick address Decrement2 ] [ text "-" ]
-      ]
-      ],
+player2Health address model =
+    div [ class "large-6 columns" ]
+    [
+    h3 [ class "subheader" ] [ text "Player 2 Health" ],
+      button [ class "success hollow button", onClick address Increment2 ] [ text "+" ],
+      h2 [  ] [ text (toString model.player2) ],
+      button [ class "alert hollow button", onClick address Decrement2 ] [ text "-" ]
+    ]
+
+reset address model =
     div [ class "row" ] [
       div [ class "large-12 large-offset-3 columns" ]
-      [
-        button [ class "warning hollow button", onClick address Reset ] [ text "reset" ]
-      ] ] ]
-    div [ class "row" ] [
-      div [ class "large-12 large-offset-3 columns" ]
-      [
-        button [ class "warning hollow button", onClick address AddCreature ] [ text "reset" ]
-      ] ] ]
+      [ button [ class "warning hollow button", onClick address Reset ] [ text "reset" ]
+      ] ]
 
-
+view : Address Action -> Model -> Html
+view address model =
+    div [ class "container" ] [
+        player1Health address model,
+        player2Health address model,
+        reset address model,
+        newCreatureForm address model,
+        creatureList address model
+       ]
 
 main: Signal Html
 main =
